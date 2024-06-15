f4e0b0bbbb7e47ca59499f9c45d009efb9083bb3
Aryan Pai <aryan.pai7@icloud.com>
2024-06-15 13:12:22 +0530

    update main.py

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT)
 
-while True:
-    lightstate = int(input())
-    match lightstate:
-        case 1:
-            GPIO.output(17, GPIO.HIGH)
-        case 0:
-            GPIO.output(17, GPIO.LOW)
+def led_on():
+    GPIO.output(17, GPIO.HIGH)
 
-GPIO.cleanup()
+def led_off():
+    GPIO.output(17, GPIO.LOW)
+
+
+if __name__ == '__main__':
+    print("Enter 1 to turn on the light, 0 to turn off the light")
+    print("Ctrl+C to exit")
+
+    while True:
+        lightstate = int(input())
+        match lightstate:
+            case 1:
+                led_on()
+            case 0:
+                led_off()
+            case 2:
+                break
+    GPIO.cleanup()
