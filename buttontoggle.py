ed5504a7ffd9f420fcd99db222d87a8f6bb335a9
Micquel D <147699544+micqueld23@users.noreply.github.com>
2024-06-17 20:27:34 +0530

    Update buttontoggle.py

--- buttontoggle.py
+++ buttontoggle.py
@@ -3,36 +3,47 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Initialising pins
+LED = 17
+button = 18
 
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Turns on LED
 def led_on():
-    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(LED, GPIO.HIGH)
     print("LED ON")
 
-
+# Turns off LED
 def led_off():
-    GPIO.output(17, GPIO.LOW)
+    GPIO.output(LED, GPIO.LOW)
     print("LED OFF")
 
-
+# Checks current state of LED, flips it
 def toggle_led():
-    ledstate = GPIO.input(17)
+    ledstate = GPIO.input(LED)
     if ledstate == 1:
         led_off()
     elif ledstate == 0:
         led_on()
 
-
+# Ensures it is run only as a script, not an import
 if __name__ == '__main__':
     print("Press the button to toggle the light")
     print("Ctrl+C to exit")
-
-    while True:
-        buttonstate = GPIO.input(18)
-        if buttonstate == 0:
-            toggle_led()
-            time.sleep(0.175)
-
-    GPIO.cleanup()
+    
+    try:
+        while True:
+            # Detects button press
+            buttonstate = GPIO.input(button)
+        
+            # Calls LED toggle function when button is pressed
+            if buttonstate == 0:
+                toggle_led()
+                time.sleep(0.175) # Debounce delay
+                
+    except KeyboardInterrupt:
+        
+        GPIO.cleanup()
