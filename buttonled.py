807ddba5c8e31f422fdeaffa7203bd2cd990b3a3
Micquel D <147699544+micqueld23@users.noreply.github.com>
2024-06-17 20:19:03 +0530

    Update buttonled.py

--- buttonled.py
+++ buttonled.py
@@ -11,20 +11,29 @@ button = 18
 GPIO.setup(LED, GPIO.OUT)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Function when button is pressed
+def buttonpress(buttonstate):
+    
+    match buttonstate:
+        case GPIO.HIGH:
+            # If button is not pressed
+            GPIO.output(17, GPIO.LOW)
+        case GPIO.LOW:
+            # If button is pressed
+            GPIO.output(17, GPIO.HIGH)
+            
 # Turning LED off to start
 GPIO.output(LED, GPIO.LOW)
 
-try:
-    while True:
-        # Checking for button press
-        buttonstate = GPIO.input(18)
+# Ensuring it is only run as a script, not an import
+if __name__ == '__main__':
 
-        match buttonstate:
-            case GPIO.HIGH:
-                # If button is not pressed
-                GPIO.output(17, GPIO.LOW)
-            case GPIO.LOW:
-                # If button is pressed
-                GPIO.output(17, GPIO.HIGH)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    try:
+        while True:
+            # Checking for button press
+            buttonstate = GPIO.input(18)
+    
+            
+    except KeyboardInterrupt:
+        # Cleans up GPIO pins / resets state when terminated using Ctrl + C
+        GPIO.cleanup()
