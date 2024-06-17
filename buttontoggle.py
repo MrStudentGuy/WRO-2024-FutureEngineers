9c63d7f984b6f1cfad3938bdb72e909e7b0288b9
Aryan Pai <aryan.pai7@icloud.com>
2024-06-17 12:19:17 +0530

    update main.py: Pai

--- buttontoggle.py
+++ buttontoggle.py
@@ -6,25 +6,24 @@ GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-led = 0
 
-def ledtoggle():
-	if(input == 1 and led != input):
-		led = input
-		led_last = not led_last
-		GPIO.output(17, int(led_last))
-		print("LED turned on? : " + str(led_last))
-	elif(input == 0 and led != input):
-		print("Toggle pin LOW")
-		led = 0
+def ledtoggle(input, led, led_last):
+    if (input == 1 and led != input):
+        led = input
+        led_last = not led_last
+        GPIO.output(17, int(led_last))
+        print("LED turned on? : " + str(led_last))
+    elif (input == 0 and led != input):
+        print("Toggle pin LOW")
+        led = 0
 
 
 if __name__ == '__main__':
-
     led = 0
     led_last = False
 
     while True:
-	    input = GPIO.input(18)
-	    ledtoggle()
+        input = GPIO.input(18)
+        ledtoggle(input, led, led_last)
+
     GPIO.cleanup()
