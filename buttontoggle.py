164d99f42df7c6b0e318154e56cef88ead1a258a
Aryan Pai <aryan.pai7@icloud.com>
2024-06-17 12:29:28 +0530

    updated something 5: Pai

--- buttontoggle.py
+++ buttontoggle.py
@@ -33,6 +33,6 @@ if __name__ == '__main__':
         buttonstate = GPIO.input(18)
         if buttonstate == 0:
             toggle_led()
-            time.sleep(0.1)
+            time.sleep(0.12)
 
     GPIO.cleanup()
