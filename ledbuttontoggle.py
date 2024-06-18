97e6817a0b26d805c21b11cd9163c81a2d75437a
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-18 11:58:51 +0530

    Traumatised by ledbuttontoggle.py

--- ledbuttontoggle.py
+++ ledbuttontoggle.py
@@ -11,39 +11,53 @@ button = 18
 GPIO.setup(LED, GPIO.OUT)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+previousstate = False
+currentstate = False
+
+
+# Only run led on/off on button press while previous state = current state
+# Make previous state = current state on button release
 # Turns on LED
 def led_on():
     GPIO.output(LED, GPIO.HIGH)
     print("LED ON")
+
 
 # Turns off LED
 def led_off():
     GPIO.output(LED, GPIO.LOW)
     print("LED OFF")
 
+
 # Checks current state of LED, flips it
-def toggle_led():
-    ledstate = GPIO.input(LED)
-    if ledstate == 1:
-        led_off()
-    elif ledstate == 0:
-        led_on()
+def toggle_led(currentstate, previousstate):
+    if currentstate == previousstate:
+        currentstate = not currentstate
+        if currentstate:
+            led_off()
+        else:
+            led_on()
+
 
 # Ensures it is run only as a script, not an import
 if __name__ == '__main__':
     print("Press the button to toggle the light")
     print("Ctrl+C to exit")
-    
+
     try:
         while True:
             # Detects button press
             buttonstate = GPIO.input(button)
-        
+
             # Calls LED toggle function when button is pressed
             if buttonstate == 0:
-                toggle_led()
-                time.sleep(0.175) # Debounce delay
-                
+                toggle_led(currentstate, previousstate)
+                time.sleep(0.175)  # Debounce delay
+                while buttonstate == 0:
+                    time.sleep(0.001)
+                if buttonstate == 1:
+                    previousstate = currentstate
+
     except KeyboardInterrupt:
         # Cleans up GPIO pins / resets state when terminated using Ctrl + C
         GPIO.cleanup()
