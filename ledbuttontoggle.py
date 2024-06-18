a0fdeb92992500c3ff7e0cb3bdf5f5cf6aa7dc37
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-18 12:32:09 +0530

    Traumatised by ledbuttontoggle.py for the FOURTH time

--- ledbuttontoggle.py
+++ ledbuttontoggle.py
@@ -6,17 +6,15 @@ GPIO.setwarnings(False)
 
 # Initialising pins
 LED = 17
-button = 18
+BUTTON = 18
 
 GPIO.setup(LED, GPIO.OUT)
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-previousstate = False
-currentstate = False
+previousstate = 0
+currentstate = 0
 
 
-# Only run led on/off on button press while previous state = current state
-# Make previous state = current state on button release
 # Turns on LED
 def led_on():
     GPIO.output(LED, GPIO.HIGH)
@@ -30,14 +28,12 @@ def led_off():
 
 
 # Checks current state of LED, flips it
-def toggle_led(currentstate, previousstate):
-    if currentstate == previousstate:
-        currentstate = not currentstate
-        if currentstate:
-            led_off()
-        else:
-            led_on()
-        previousstate = currentstate
+def toggle_led():
+    ledstate = GPIO.input(LED)
+    if ledstate == 1:
+        led_off()
+    else:
+        led_on()
 
 
 # Ensures it is run only as a script, not an import
@@ -48,17 +44,10 @@ if __name__ == '__main__':
     try:
         while True:
             # Detects button press
-            buttonstate = GPIO.input(button)
-
-            # Calls LED toggle function when button is pressed
-            while buttonstate == 0:
-                print ('button pressed')
-                if buttonstate == 1:
-                    print ('button released')
-                    toggle_led(currentstate, previousstate)
-                    time.sleep(0.175)  # Debounce delay
-
-
+            currentstate = GPIO.input(BUTTON)
+            if previousstate == 1 and currentstate == 0:
+                toggle_led()
+            previousstate = currentstate
     except KeyboardInterrupt:
         # Cleans up GPIO pins / resets state when terminated using Ctrl + C
         GPIO.cleanup()
