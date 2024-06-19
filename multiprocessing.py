3bd626382ee75dc8b0de30f1ba9d71a0fabe0d8c
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-19 11:34:26 +0530

    Created multiprocessing.py

---
+++ multiprocessing.py
@@ -0,0 +1,38 @@
+from multiprocessing import Process
+import os
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+
+def count1():
+    i = 0
+    for i in range(10):
+        print(i)
+        time.sleep(1)
+
+
+def count2():
+    i = 10
+    for i in range(10):
+        print(i)
+        time.sleep(1)
+
+
+# Ensures it is run only as a script, not an import
+if __name__ == '__main__':
+
+    try:
+        while True:
+
+            c1 = Process(target = count1(), args=())
+            c2 = Process(target=count2(), args=())
+
+            c1.start()
+            c2.start()
+
+    except KeyboardInterrupt:
+        # Cleans up GPIO pins / resets state when terminated using Ctrl + C
+        GPIO.cleanup()
