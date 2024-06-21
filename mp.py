a7247d4b49d2a2bb0e177c010af381e3390bf179
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-21 11:24:34 +0530

    Updated up.py

--- mp.py
+++ mp.py
@@ -6,34 +6,27 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+number = 0
 
-def count1(q):
+
+def count1():
     for i in range(0, 10):
-        print(f"1: {i}")
-        time.sleep(1)
-        q.put(f"2: {i}")
+        number = i
 
 
-def count2(q):
-    while True:
-        data = q.get()  # Receive message from queue
-        if data == 'done':
-            break
-        print(f"Received from 1: {data}")
-        time.sleep(1)
+def count2():
+    for i in range (0, 10):
+        print(number)
 
 
 # Ensures it is run only as a script, not an import
 if __name__ == '__main__':
 
-    q = queue.Queue()
-
-    c1 = multiprocessing.Process(target=count1, args=(q,))
-    c2 = multiprocessing.Process(target=count2, args=(q,))
+    c1 = multiprocessing.Process(target=count1)
+    c2 = multiprocessing.Process(target=count2)
 
     c1.start()
     c2.start()
 
     c1.join()  # Wait for process 1 to finish
-    q.put('done')
     c2.join()  # Wait for process 2 to finish
