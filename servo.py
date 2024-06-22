2db228f850ba6e189cdb58fde6ae5bcca3ae9a28
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-22 12:11:13 +0530

    Updated servo.py

--- servo.py
+++ servo.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     try:
         while True:
             angle = int(input())
-            pwm.changeDutyCycle(angle/18 + 2)
+            pwm.ChangeDutyCycle(angle/18 + 2)
 
     except KeyboardInterrupt:
         # Cleans up GPIO pins / resets state and terminates PWM when terminated using Ctrl + C
