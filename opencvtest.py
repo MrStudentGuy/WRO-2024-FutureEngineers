d354c2906b486ad699adf7d7ec01d2864c9a8465
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-24 13:40:01 +0530

    Updated opencvtest.py

--- opencvtest.py
+++ opencvtest.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         while True:
             frame = cam.capture_array() # Constantly capturing frames / video
             cv2.imshow('Video', frame) # Creating OpenCV preview using captured frames, named 'Video'
-            cv2.waitKey(0) # Allows stream to continue until a key is pressed / program terminated
+            cv2.waitKey(1) # Allows stream to continue until a key is pressed / program terminated
     except KeyboardInterrupt:
         # Stops camera feed when Ctrl + C is pressed
         frame = cam.capture_array()
