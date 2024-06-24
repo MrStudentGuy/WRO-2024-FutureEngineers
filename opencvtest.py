eaca2b1a8716d741def0478deb6e617cdd71f614
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-24 14:21:46 +0530

    Updated opencvtest.py

--- opencvtest.py
+++ opencvtest.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     try:
         while True:
             frame = cam.capture_array() # Constantly capturing frames / video
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # Converts each frame to HSV colour
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Converts each frame to HSV colour
             contours, hierarchy = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
             cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
             cv2.imshow('Video', frame) # Creating OpenCV preview using captured frames, named 'Video'
