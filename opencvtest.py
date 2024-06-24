9e60c38b76c414ed4f5189ad180b961aba0ab536
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-24 13:38:30 +0530

    Updated opencvtest.py

--- opencvtest.py
+++ opencvtest.py
@@ -1,11 +1,18 @@
-import time
-from picamera2 import Picamera2, MappedArray
+from picamera2 import Picamera2
 import cv2
 
 # Initialising and starting picamera2 object
 cam = Picamera2()
 cam.start()
 
-while True:
-    frame = cam.capture_array() # Constantly capturing frames / video
-    cv2.imshow('Video', frame) # Creating OpenCV preview using captured frames, named 'Video'
+if __name__ == '__main__':
+    try:
+        while True:
+            frame = cam.capture_array() # Constantly capturing frames / video
+            cv2.imshow('Video', frame) # Creating OpenCV preview using captured frames, named 'Video'
+            cv2.waitKey(0) # Allows stream to continue until a key is pressed / program terminated
+    except KeyboardInterrupt:
+        # Stops camera feed when Ctrl + C is pressed
+        frame = cam.capture_array()
+        cam.stop()
+        cv2.destroyAllWindows()
