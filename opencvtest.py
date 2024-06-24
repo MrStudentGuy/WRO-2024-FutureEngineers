bc572c755b443c4b6ece548c51182191aecad05b
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-24 14:03:14 +0530

    Updated opencvtest.py

--- opencvtest.py
+++ opencvtest.py
@@ -11,12 +11,10 @@ if __name__ == '__main__':
     try:
         while True:
             frame = cam.capture_array() # Constantly capturing frames / video
-            frameUMat = np.float32(frame)
-            rgb_frame = cv2.cvtColor(frameUMat, cv2.COLOR_BGR2RGB)
-            cv2.imshow('Video', rgb_frame) # Creating OpenCV preview using captured frames, named 'Video'
+            cv2.imshow('Video', frame) # Creating OpenCV preview using captured frames, named 'Video'
             cv2.waitKey(1) # Allows stream to continue until a key is pressed / program terminated
     except KeyboardInterrupt:
         # Stops camera feed when Ctrl + C is pressed
-        rgb_frame = cam.capture_array()
+        frame = cam.capture_array()
         cam.stop()
         cv2.destroyAllWindows()
