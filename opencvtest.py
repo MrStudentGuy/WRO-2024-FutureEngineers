36928ef3d5408e84a50c98fe72706b9c9b8e9ed9
micqueld23 <devlaliwallamicquel@gmail.com>
2024-06-24 14:31:50 +0530

    Updated opencvtest.py

--- opencvtest.py
+++ opencvtest.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
             contours, hierarchy = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
             if not contours:
                 print("No contours found!")
-            cv2.drawContours(frame, contours, -1, (0, 0, 0), 3)
+            cv2.drawContours(frame, contours, -1, (255, 255, 255), 3)
             cv2.imshow('Video', frame) # Creating OpenCV preview using captured frames, named 'Video'
             cv2.waitKey(1) # Allows stream to continue until a key is pressed / program terminated
     except KeyboardInterrupt:
