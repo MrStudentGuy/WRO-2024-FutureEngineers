a3be30666e8f69d3a47c80f0c53090742978cc4c
Aryan Pai <aryan.pai7@icloud.com>
2024-06-27 12:48:48 +0530

    Quadrature Encoder on motor

--- EncoderMotor.py
+++ EncoderMotor.py
@@ -1,66 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 
-# Pin numbers on Raspberry Pi
-CLK_PIN = 7   # GPIO7 connected to the rotary encoder's CLK pin
-DT_PIN = 8    # GPIO8 connected to the rotary encoder's DT pin
-SW_PIN = 25   # GPIO25 connected to the rotary encoder's SW pin
-
-DIRECTION_CW = 0
-DIRECTION_CCW = 1
-
-counter = 0
-direction = DIRECTION_CW
-CLK_state = 0
-prev_CLK_state = 0
-
-button_pressed = False
-prev_button_state = GPIO.HIGH
-
-# Configure GPIO pins
+# Set up GPIO pins for encoder channels A and B
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(CLK_PIN, GPIO.IN)
-GPIO.setup(DT_PIN, GPIO.IN)
-GPIO.setup(SW_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Channel A
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Channel B
 
-# Read the initial state of the rotary encoder's CLK pin
-prev_CLK_state = GPIO.input(CLK_PIN)
+# Initialize encoder counter
+encoder_count = 0
+
+# Define a function to handle encoder interrupts
+def encoder_interrupt(channel):
+    global encoder_count
+    if channel == 27:  # Channel A
+        if GPIO.input(23):  # Channel B
+            encoder_count += 1
+        else:
+            encoder_count -= 1
+    elif channel == 23:  # Channel B
+        if GPIO.input(27):  # Channel A
+            encoder_count -= 1
+        else:
+            encoder_count += 1
+
+# Set up interrupts for encoder channels A and B
+GPIO.add_event_detect(27, GPIO.RISING, callback=encoder_interrupt, bouncetime=10)
+GPIO.add_event_detect(23, GPIO.RISING, callback=encoder_interrupt, bouncetime=10)
 
 try:
     while True:
-        # Read the current state of the rotary encoder's CLK pin
-        CLK_state = GPIO.input(CLK_PIN)
-
-        # If the state of CLK is changed, then pulse occurred
-        # React to only the rising edge (from LOW to HIGH) to avoid double count
-        if CLK_state != prev_CLK_state and CLK_state == GPIO.HIGH:
-            # If the DT state is HIGH, the encoder is rotating in counter-clockwise direction
-            # Decrease the counter
-            if GPIO.input(DT_PIN) == GPIO.HIGH:
-                counter -= 1
-                direction = DIRECTION_CCW
-            else:
-                # The encoder is rotating in clockwise direction => increase the counter
-                counter += 1
-                direction = DIRECTION_CW
-
-            print("Rotary Encoder:: direction:", "CLOCKWISE" if direction == DIRECTION_CW else "ANTICLOCKWISE",
-                  "- count:", counter)
-
-        # Save last CLK state
-        prev_CLK_state = CLK_state
-
-        # State change detection for the button
-        button_state = GPIO.input(SW_PIN)
-        if button_state != prev_button_state:
-            time.sleep(0.01)  # Add a small delay to debounce
-            if button_state == GPIO.LOW:
-                print("The button is pressed")
-                button_pressed = True
-            else:
-                button_pressed = False
-
-        prev_button_state = button_state
+        print("Encoder count:", encoder_count)
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()  # Clean up GPIO on program exit
+    GPIO.cleanup()
