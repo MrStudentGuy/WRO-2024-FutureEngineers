95689396f47b12e76b246d0cffaddf8a7602608f
Aryan Pai <aryan.pai7@icloud.com>
2024-06-27 13:10:02 +0530

    Paach rupay ki pepsi, giri bhai sexy

--- EncoderMotor.py
+++ EncoderMotor.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO
+import gpiozero
+from signal import pause
 import time
 
 # Constants
@@ -6,46 +8,49 @@ CHANNEL_A_PIN = 23
 CHANNEL_B_PIN = 27
 BOUNCE_TIME = 10
 
-# Set up GPIO pins for encoder channels A and B
-GPIO.setmode(GPIO.BCM)
 
-# Set GPIO mode to input explicitly
-GPIO.setup(CHANNEL_A_PIN, GPIO.IN)
-GPIO.setup(CHANNEL_B_PIN, GPIO.IN)
+# Define the encoder pins
+encoder_a = gpiozero.DigitalInputDevice(23)  # Pin 23 for encoder A
+encoder_b = gpiozero.DigitalInputDevice(27)  # Pin 27 for encoder B
 
-# Set pull-up resistors
-GPIO.setup(CHANNEL_A_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(CHANNEL_B_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Initialize the counter
+counter = 0
 
-# Initialize encoder counter
-encoder_count = 0
+def encoder_a_rise():
+    global counter
 
-# Define a function to handle encoder interrupts
-def encoder_interrupt(channel):
-    global encoder_count
-    if channel == CHANNEL_A_PIN:  # Channel A
-        if GPIO.input(CHANNEL_B_PIN):  # Channel B
-            encoder_count += 1
-        else:
-            encoder_count -= 1
-    elif channel == CHANNEL_B_PIN:  # Channel B
-        if GPIO.input(CHANNEL_A_PIN):  # Channel A
-            encoder_count -= 1
-        else:
-            encoder_count += 1
+    if encoder_b.value:
+        counter += 1
+    else:
+        counter -= 1
 
-# Set up interrupts for encoder channels A and B
-try:
-    GPIO.add_event_detect(CHANNEL_A_PIN, GPIO.RISING, callback=encoder_interrupt, bouncetime=BOUNCE_TIME)
-    GPIO.add_event_detect(CHANNEL_B_PIN, GPIO.RISING, callback=encoder_interrupt, bouncetime=BOUNCE_TIME)
-except RuntimeError as e:
-    print("Error adding edge detection:", e)
-    exit(1)
+
+# def encoder_counter():
+#     global counter
+#     if encoder_a.value:
+#         counter += 1
+#     else:
+#         counter -= 1
+
+def encoder_b_rise():
+    global counter
+
+    if encoder_a.value:
+        counter -= 1
+
+    else:
+        counter += 1
+
+# Set up the encoder pins to trigger on rising edges
+encoder_a.when_activated = encoder_a_rise
+encoder_b.when_activated = encoder_b_rise
+
+print("Encoder counter started. Press Ctrl+C to exit.")
 
 try:
     while True:
-        print("Encoder count:", encoder_count)
         time.sleep(0.1)
-
+        print("counter A: {}".format(counter))
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    print("Encoder counter stopped.")
+    print("Final counter value:", counter)
